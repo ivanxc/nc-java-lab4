605e928ea9f95a504487b47a868481193e810a1f
Ivan <djsupersky@gmail.com>
2022-05-10 18:52:46 +0300

    delete redundant files

--- src/main/java/com/ivanxc/netcracker/lab/CollectionsApplication.java
+++ src/main/java/com/ivanxc/netcracker/lab/CollectionsApplication.java
@@ -5,44 +5,27 @@ import com.ivanxc.netcracker.lab.analytics.PerformanceDetails;
 import com.ivanxc.netcracker.lab.analytics.visualisation.DocumentReporter;
 import java.io.FileNotFoundException;
 import java.util.List;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import java.io.IOException;
 
-public class CollectionsApplication extends Application {
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-            CollectionsApplication.class.getResource("collections-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class CollectionsApplication {
 
     public static void main(String[] args) {
-//        launch();
         showCollectionsPerformance(1);
-        System.out.println("END");
     }
 
     public static void showCollectionsPerformance(int precision) {
         CollectionBenchmarker.setPrecision(precision);
 
-        List<PerformanceDetails> listRes = CollectionBenchmarker.measureLists(100, 1000, 5000, 10000, 20000, 30000);
+        List<PerformanceDetails> listRes = CollectionBenchmarker.measureLists(1000, 10000, 50000, 100000, 200000);
         for(PerformanceDetails details : listRes) {
             System.out.println(details);
         }
 
-        List<PerformanceDetails> setRes = CollectionBenchmarker.measureSets(100, 1000, 5000, 10000, 20000, 30000);
+        List<PerformanceDetails> setRes = CollectionBenchmarker.measureSets(1000, 10000, 50000, 100000, 200000);
         for(PerformanceDetails details : setRes) {
             System.out.println(details);
         }
 
-        List<PerformanceDetails> mapRes = CollectionBenchmarker.measureMaps(100, 1000, 5000, 10000, 20000, 30000);
+        List<PerformanceDetails> mapRes = CollectionBenchmarker.measureMaps(1000, 10000, 50000, 100000, 200000);
         for(PerformanceDetails details : mapRes) {
             System.out.println(details);
         }
